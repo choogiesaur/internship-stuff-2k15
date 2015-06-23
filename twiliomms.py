b09536dcdeecfe01f9ae18d365ec0ea8b2302026
Firas S. <choogiesaur@gmail.com>
2015-06-23 13:13:40 -0400

    Create twiliomms.py
    code to test twilio picture MMS sending

---
+++ twiliomms.py
@@ -0,0 +1,15 @@
+from twilio.rest import TwilioRestClient
+
+#these are the LIVE credentials!!!
+account = "ACab6ad52e72143acd38066c67d687496e"
+token = "ec33fd1862a7700578ef67050cdba290"
+
+#these are the TEST credentials
+#account = "ACdc5eafe1d3133241cfc9bd53f23a2785"
+#token = "1b354730c3cb7981d6963ed0f80c8f42"
+
+client = TwilioRestClient(account, token)
+
+message = client.messages.create(to="+17326820887", from_="+17323336281",
+    body="IDT Twilio MMS test",
+    media_url=['http://i.imgur.com/CqNES4i.png'])
