821971b75d6b6eb85a987701807e1af731652207
Firas S. <choogiesaur@gmail.com>
2015-06-23 12:19:47 -0400

    Create twiliosms.py
    test code for sending SMS

---
+++ twiliosms.py
@@ -0,0 +1,16 @@
+from twilio.rest import TwilioRestClient
+
+print("Hello")
+
+#these are the LIVE credentials!!!
+account = "ACab6ad52e72143acd38066c67d687496e"
+token = "ec33fd1862a7700578ef67050cdba290"
+
+#these are the TEST credentials
+#account = "ACdc5eafe1d3133241cfc9bd53f23a2785"
+#token = "1b354730c3cb7981d6963ed0f80c8f42"
+
+client = TwilioRestClient(account, token)
+
+message = client.messages.create(to="+7326820887", from_="+17323336281",
+    body="Hello from IDT!!!")
