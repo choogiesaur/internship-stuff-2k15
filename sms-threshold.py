a27b13d5088bc3be0604ac4c9a8be0c75e614be7
Firas S. <choogiesaur@users.noreply.github.com>
2015-06-24 16:18:49 -0400

    Update and rename sms-logic.py to sms-threshold.py
    POC for sending message based on certain conditions.

---
+++ sms-threshold.py
@@ -0,0 +1,25 @@
+from twilio.rest import TwilioRestClient
+import random
+
+#these are the LIVE credentials!!!
+account = "ACab6ad52e72143acd38066c67d687496e"
+token   = "ec33fd1862a7700578ef67050cdba290"
+twilio_num 	= "+17323336281"
+recipient 	= "+17326820887"
+message 	= ""
+
+client = TwilioRestClient(account, token)
+
+threshold = 50
+num = random.randint(0,100)
+
+#num exceeds threshold, send alert
+if num > threshold:
+	message = "Alert! Value " + str(num) + " is > than the threshold " + str(threshold)
+	print(message)
+	sms = client.messages.create(to=recipient, from_=twilio_num, body=message)
+#number is safe. of course you can exclude this if you just need alerts.
+elif num <= threshold:
+	message = "Safe. Value " + str(num) + " is <= the threshold " + str(threshold)
+	print(message)
+	sms = client.messages.create(to=recipient, from_=twilio_num, body=message)
