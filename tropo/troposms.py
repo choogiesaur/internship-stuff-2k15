c8f9290f5f3f564fd16406a60b73aaf473a8c473
Firas S <choogiesaur@gmail.com>
2015-06-30 14:27:39 -0400

    tropo, wireshark
    testing tropo and wrote wireshark script

--- tropo/troposms.py
+++ tropo/troposms.py
@@ -1,5 +1,29 @@
-from tropo import all
+# tests fix of gh-14 for "_from" parameter in the "message" function. 
+# Proposed convention is to use "_from" as the parameter
+# so as not to conflict with "from" Python reserved word.
 
-event = call("+14075550100", {
-	"network":"SMS"})
-event.value.say("Don't forget your meeting at 2 p.m. on Wednesday!")
+# _from arg works
+
+# Invoke using a token
+
+# Sample application using the itty-bitty python web framework from:
+#  http://github.com/toastdriven/itty
+
+from itty import *
+from tropo import Tropo, Session
+
+TO_NUMBER = '17326820887'
+FROM_NUMBER = '12012895306'
+
+
+@post('/index.json')
+def index(request):
+        t = Tropo()
+        t.message("Hello World", TO_NUMBER, channel='VOICE', _from='tel:+' + FROM_NUMBER)
+	json = t.RenderJson()
+	print json
+	return json
+#retest
+
+
+run_itty()
--- twilio/wireshark-trace.py
+++ twilio/wireshark-trace.py
@@ -1,7 +1,7 @@
 from twilio.rest import TwilioRestClient
 from time import gmtime, strftime
 
-print("Twilio message test")
+print("Twilio message test at: ", strftime("%Y-%m-%d %H:%M:%S", gmtime()))
 
 #these are the LIVE credentials!!!
 account = "ACab6ad52e72143acd38066c67d687496e"
@@ -13,5 +13,6 @@ token   = "ec33fd1862a7700578ef67050cdba290"
 
 client = TwilioRestClient(account, token)
 
+txt = "tracing this with wireshark @ " + strftime("%Y-%m-%d %H:%M:%S", gmtime())
 message = client.messages.create(to="+17326820887", from_="+17323336281",
-    body="Tracing this with wireshark!!!")
+    body=txt)
