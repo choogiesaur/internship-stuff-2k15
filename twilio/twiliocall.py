c9ba47cd0db1a2d6393a797ca49e0d21a9b7f94f
Firas S. <choogiesaur@users.noreply.github.com>
2015-08-11 10:51:07 -0400

    Update twiliocall.py

--- twilio/twiliocall.py
+++ twilio/twiliocall.py
@@ -7,8 +7,8 @@ token 	= "ec33fd1862a7700578ef67050cdba290"
 
 client = TwilioRestClient(account, token)
 
-call = client.calls.create(to="7326820887",
-		from_="7323336281",
+call = client.calls.create(to="xxxxxxxxxx",
+		from_="xxxxxxxxxx",
 		url="http://twimlets.com/holdmusic?Bucket=com.twilio.music.ambient")
 
 print(call.sid)
