73ed63ee7eb8d636d05a89189b37d3f99bc57012
Firas S. <choogiesaur@users.noreply.github.com>
2015-08-11 10:51:32 -0400

    Update twiliomms.py

--- twilio/twiliomms.py
+++ twilio/twiliomms.py
@@ -7,6 +7,6 @@ token   = "ec33fd1862a7700578ef67050cdba290"
 
 client = TwilioRestClient(account, token)
 
-message = client.messages.create(to="+17326820887", from_="+17323336281",
+message = client.messages.create(to="+1xxxxxxxxxx", from_="+1xxxxxxxxxx",
     body		="IDT Twilio MMS test",
     media_url	=['http://i.imgur.com/CqNES4i.png'])
